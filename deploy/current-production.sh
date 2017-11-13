dd151771a6f815951ab20025ca4daa8204fe8e91
Vitaly Nikolaiev <vitaly.nikolaiev@datarobot.com>
2017-11-13 15:54:38 +0200

    change scripts

--- deploy/current-production.sh
+++ deploy/current-production.sh
@@ -1,4 +1,5 @@
-#!/usr/bin/env bash
+#!/bin/bash
+cd wp-content/themes/$THEME_NAME
 yarn prod
 git push prod $CI_BRANCH
-sshpass -p ${SFTP_PASS} sftp -P2222 ${SFTP_USERNAME}@${WPE_INSTALL}.sftp.wpengine.com:wp-content/themes/${THEME_NAME} <<< $'cd assets\n mkdir built\n put -r assets/built\n'
+sshpass -p ${SFTP_PASS} sftp -P2222 ${SFTP_USERNAME}@${WPE_INSTALL}.sftp.wpengine.com:wp-content/themes/${THEME_NAME} <<< $'cd assets\n mkdir built\n put -r assets/built'
